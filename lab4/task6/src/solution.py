3c6a02550f68bd2af053a9a3b5a84bbb24b57b3a
Nick <ronilovo2@gmail.com>
2025-06-11 08:18:19 +0300

    docs: add comments to lab4/task6

--- lab4/task6/src/solution.py
+++ lab4/task6/src/solution.py
@@ -9,28 +9,36 @@ output_path = "../txtf/output.txt"
 @measure_time_and_memory
 def z_func(s):
     n = len(s)
-    z = [0] * n
-    l, r = 0, 0
+    z = [0] * n # Массив Z-функции
+    l, r = 0, 0 # Границы самого правого отрезка совпадения
+
     for i in range(1, n):
+        # Если текущий индекс внутри отрезка [l, r], берем минимум из оставшейся длины отрезка и z[i-l]
         if i <= r:
             z[i] = min(r - i + 1, z[i - l])
+
+        # Пытаемся расширить текущее совпадение вручную
         while i + z[i] < n and s[z[i]] == s[i + z[i]]:
             z[i] += 1
+
+        # Если нашли более правый отрезок совпадения, то обновляем границы
         if i + z[i] - 1 > r:
             l, r = i, i + z[i] - 1
+
     return z[1:]
 
 
 def main():
-    # Reading input file
+    # Чтение входных данных из файла
     input_data = read_from_file(input_path)
 
-    # Algorithm
+    # Извлечение параметров из входных данных
     s = input_data
 
+    # Вычисление результата
     result = z_func(s)
 
-    # Writing output file
+    # Запись результата в выходной файл
     output_data = " ".join(str(x) for x in result)
     write_to_file(output_path, output_data)
 
