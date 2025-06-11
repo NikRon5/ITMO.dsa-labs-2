ed42b45a880f7e4597034ae943380ebb6900f151
Nick <ronilovo2@gmail.com>
2025-06-11 08:17:16 +0300

    docs: add comments to lab1/task8

--- lab1/task8/src/solution.py
+++ lab1/task8/src/solution.py
@@ -1,34 +1,42 @@
 from utils.file_utlis import read_from_file, write_to_file
 from utils.time_memory_utlis import measure_time_and_memory
 
+
 input_path = "../txtf/input.txt"
 output_path = "../txtf/output.txt"
 
 
 @measure_time_and_memory
 def max_lectures(requests):
+    # Сортируем лекции по времени окончания
     requests.sort(key=lambda x: x[1])
 
-    c = 0
-    last_end = 0
+    c = 0 # Счетчик выбранных лекций
+    last_end = 0 # Время окончания последней выбранной лекции
+
     for start, end in requests:
+        # Если текущая лекция начинается после окончания последней выбранной
         if start >= last_end:
-            c += 1
-            last_end = end
+            c += 1 # Увеличиваем счетчик
+            last_end = end # Обновляем время окончания последней лекции
     return c
 
+
 def main():
-    # Reading input file
+    # Чтение входных данных из файла
     input_data = read_from_file(input_path)
 
-    # Algorithm
+    # Извлечение параметров из входных данных
     n = input_data[0]
     requests = input_data[1:]
+
+    # Вычисление результата
     result = max_lectures(requests)
 
-    # Writing output file
+    # Запись результата в выходной файл
     output_data = str(result)
     write_to_file(output_path, output_data)
+
 
 if __name__ == "__main__":
     main()
