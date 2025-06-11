59674ee70384e127a0cfd92971da0c123b1b6735
Nick <ronilovo2@gmail.com>
2025-06-11 08:18:12 +0300

    docs: add comments to lab4/task5

--- lab4/task5/src/solution.py
+++ lab4/task5/src/solution.py
@@ -9,27 +9,36 @@ output_path = "../txtf/output.txt"
 @measure_time_and_memory
 def prefix_func(s):
     n = len(s)
-    prefix = [0] * n
+    prefix = [0] * n # Массив префикс-функции
+
     for i in range(1, n):
-        j = prefix[i - 1]
+        j = prefix[i - 1] # Начинаем с предыдущего значения префикс-функции
+
+        # Пока не найдем совпадение или не дойдем до начала
         while j > 0 and s[i] != s[j]:
-            j = prefix[j - 1]
+            j = prefix[j - 1] # "Откатываем" j по префикс-функции
+
+        # Если символы совпали, увеличиваем длину совпадающего префикса
         if s[i] == s[j]:
             j += 1
+
+        # Записываем полученное значение
         prefix[i] = j
+
     return prefix
 
 
 def main():
-    # Reading input file
+    # Чтение входных данных из файла
     input_data = read_from_file(input_path)
 
-    # Algorithm
+    # Извлечение параметров из входных данных
     s = input_data
 
+    # Вычисление результата
     result = prefix_func(s)
 
-    # Writing output file
+    # Запись результата в выходной файл
     output_data = " ".join(str(x) for x in result)
     write_to_file(output_path, output_data)
 
