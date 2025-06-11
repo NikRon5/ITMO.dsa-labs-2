d09f0ec9813f600d9c6fa0e4c144bd9867365d88
Nick <ronilovo2@gmail.com>
2025-06-11 06:49:49 +0300

    feat: lab4/task1

---
+++ lab4/task1/src/solution.py
@@ -0,0 +1,39 @@
+from utils.file_utlis import read_from_file, write_to_file
+from utils.time_memory_utlis import measure_time_and_memory
+
+
+input_path = "../txtf/input.txt"
+output_path = "../txtf/output.txt"
+
+
+@measure_time_and_memory
+def naive_pattern_search(p, t):
+    len_p = len(p)
+    occurrences = []
+    for i in range(len(t) - len_p + 1):
+        if t[i:i + len_p] == p:
+            occurrences.append(i + 1)
+
+    result = [len(occurrences), occurrences]
+
+    return result
+
+
+def main():
+    # Reading input file
+    input_data = read_from_file(input_path)
+
+    # Algorithm
+    p = input_data[0]
+    t = input_data[1]
+
+    result = naive_pattern_search(p, t)
+
+    # Writing output file
+    output_data = str(result[0]) + "\n"
+    output_data += " ".join(str(x) for x in result[1])
+    write_to_file(output_path, output_data)
+
+
+if __name__ == "__main__":
+    main()
