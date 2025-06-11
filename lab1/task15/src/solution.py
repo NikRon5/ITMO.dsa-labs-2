e387af93297e66483d440e60d59db53470232fa2
Nick <ronilovo2@gmail.com>
2025-06-11 08:17:22 +0300

    docs: add comments to lab1/task15

--- lab1/task15/src/solution.py
+++ lab1/task15/src/solution.py
@@ -8,38 +8,43 @@ output_path = "../txtf/output.txt"
 
 @measure_time_and_memory
 def remove_brackets(s):
+    # Открывающие скобки соответствующие закрывающим
     pairs = {
         ")": "(",
         "]": "[",
         "}": "{"
     }
-    stack = []
-    remove = set()
+    stack = [] # Стек для хранения индексов открывающих скобок
+    remove = set() # Множество для хранения индексов скобок, которые нужно удалить
 
     for i, char in enumerate(s):
-        if char in '([{':
+        if char in '([{': # Если это открывающая скобка, добавляем ее индекс в стек
             stack.append(i)
         elif char in ')]}':
-            if stack and s[stack[-1]] == pairs[char]:
-                stack.pop()
+            if stack and s[stack[-1]] == pairs[char]: # Если пара найдена
+                stack.pop() # Удаляем ее из стека
             else:
-                remove.add(i)
+                remove.add(i) # Помечаем закрывающую скобку для удаления
 
+    # Открывающие скобки не имеющие пары помечаем для удаления
     remove.update(stack)
 
+    # Исключаем помеченные для удаления скобки
     result = ''.join(s[i] for i in range(len(s)) if i not in remove)
     return result
 
 
 def main():
-    # Reading input file
+    # Чтение входных данных из файла
     input_data = read_from_file(input_path)
 
-    # Algorithm
+    # Извлечение параметров из входных данных
     s = input_data
+
+    # Вычисление результата
     result = remove_brackets(s)
 
-    # Writing output file
+    # Запись результата в выходной файл
     output_data = result
     write_to_file(output_path, output_data)
 
