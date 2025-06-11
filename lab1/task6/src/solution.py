bb1d22c7eacd8874b1ba2c4fb8536a7466314b09
Nick <ronilovo2@gmail.com>
2025-06-11 08:17:10 +0300

    docs: add comments to lab1/task6

--- lab1/task6/src/solution.py
+++ lab1/task6/src/solution.py
@@ -1,29 +1,41 @@
 from utils.file_utlis import read_from_file, write_to_file
 from utils.time_memory_utlis import measure_time_and_memory
 
+
 input_path = "../txtf/input.txt"
 output_path = "../txtf/output.txt"
 
 
 @measure_time_and_memory
 def largest_number(nums):
+    # Преобразуем числа в строки
     nums = list(map(str, nums))
+
+    # Сортировка: умножаем строковое представление числа на 4 и сортируем в обратном порядке
     nums.sort(key=lambda x: x * 4, reverse=True)
+
+    # Объединяем отсортированные числа в строку
     max_num = "".join(nums)
+
+    # Если все числа нули
     return max_num if max_num[0] != "0" else "0"
 
+
 def main():
-    # Reading input file
+    # Чтение входных данных из файла
     input_data = read_from_file(input_path)
 
-    # Algorithm
+    # Извлечение параметров из входных данных
     n = input_data[0]
     nums = input_data[1]
+
+    # Вычисление результата
     result = largest_number(nums)
 
-    # Writing output file
+    # Запись результата в выходной файл
     output_data = result
     write_to_file(output_path, output_data)
+
 
 if __name__ == "__main__":
     main()
