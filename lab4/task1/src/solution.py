f42116b3b024b2130336a105818c8b6c9151d60c
Nick <ronilovo2@gmail.com>
2025-06-11 08:18:06 +0300

    docs: add comments to lab4/task1

--- lab4/task1/src/solution.py
+++ lab4/task1/src/solution.py
@@ -9,27 +9,28 @@ output_path = "../txtf/output.txt"
 @measure_time_and_memory
 def naive_pattern_search(p, t):
     len_p = len(p)
-    occurrences = []
+    occurrences = [] # Позиции вхождения
+
     for i in range(len(t) - len_p + 1):
+        # Сравниваем подстроку текста с искомой подстрокой
         if t[i:i + len_p] == p:
-            occurrences.append(i + 1)
+            occurrences.append(i + 1) # Добавляем
 
-    result = [len(occurrences), occurrences]
-
-    return result
+    return [len(occurrences), occurrences]
 
 
 def main():
-    # Reading input file
+    # Чтение входных данных из файла
     input_data = read_from_file(input_path)
 
-    # Algorithm
+    # Извлечение параметров из входных данных
     p = input_data[0]
     t = input_data[1]
 
+    # Вычисление результата
     result = naive_pattern_search(p, t)
 
-    # Writing output file
+    # Запись результата в выходной файл
     output_data = str(result[0]) + "\n"
     output_data += " ".join(str(x) for x in result[1])
     write_to_file(output_path, output_data)
